4419d3ae375b634bdab418c160bb3105831ceb77
fsun10-su <fsun10@syr.edu>
2024-09-04 16:41:34 -0500

    studio 1

--- src/studio1/ice_cream.java
+++ src/studio1/ice_cream.java
@@ -5,12 +5,13 @@ public class ice_cream {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	    /*
-	    int
-	    boolean
-	    int
-	    double
-	    string
-	    double
+	    1.int
+	    2.boolean
+	    3.int
+	    4.double
+	    5.string
+	    6.string
+	    7.double
 	     */
 
 	}
